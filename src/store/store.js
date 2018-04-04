ef7c7fe9e78e02b1a51c899a0e22834684db04f1
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-04-03 18:37:46 -0700

    minimun req

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
+import data from '../data/exampleVideoData.js';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const initialState = {
+  videoList: data,
+  currentVideo: data[0]
+};
+
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 export default store;
